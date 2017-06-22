fe779490cea0a8a8c7e6c5467b9062e3f465927e
packowitz <PhilippSchum@web.de>
2017-06-22 15:31:38 +1200

    enable notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import {Firebase} from "@ionic-native/firebase";
 
 @Component({
   templateUrl: 'app.html'
@@ -11,12 +12,28 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public firebase: Firebase) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.configureFirebase();
     });
+  }
+
+  public configureFirebase() {
+    try {
+      this.firebase.getToken()
+        .then(token => {})
+        .catch(err => console.log("Error on FirebasePlugin.getToken: " + err));
+      this.firebase.onTokenRefresh()
+        .subscribe(token => {});
+
+      this.firebase.subscribe("emns").then(response => {});
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {Firebase} from "@ionic-native/firebase";
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Firebase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
