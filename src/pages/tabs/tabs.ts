bbc79b0f431b0a5b374966a8bd76a9e538ace6b7
packowitz <PhilippSchum@web.de>
2017-06-22 13:07:40 +1200

    styling home screen

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -9,9 +9,9 @@ import { HomePage } from '../home/home';
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  home = HomePage;
+  studies = AboutPage;
+  atAuckland = ContactPage;
 
   constructor() {
 
